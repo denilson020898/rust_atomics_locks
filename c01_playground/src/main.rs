1de08305df30bab4d335759eac8317d73097643e
Denilson <denilson020898@gmail.com>
2024-01-03 23:39:47 +0700

    update

--- c01_playground/src/main.rs
+++ c01_playground/src/main.rs
@@ -1,6 +1,9 @@
 use std::thread;
 
+
+
 fn main() {
+    // test 1
     let h1 = thread::spawn(f);
     let h2 = thread::spawn(f);
 
@@ -8,8 +11,26 @@ fn main() {
 
     let a = h1.join().unwrap();
     let b = h2.join().unwrap();
-
     println!("result of a = {a}, and b = {b}");
+
+    // test 2
+    let numbers = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        for n in numbers {
+            println!("{n}");
+        }
+    });
+    handle.join().unwrap();
+
+    // test 3
+    let numbers = Vec::from_iter(0..=1000);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        sum / len
+    });
+    let average = t.join().unwrap();
+    println!("average is {average}");
 }
 
 fn f() -> bool {
