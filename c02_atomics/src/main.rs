7f7a35f737652b44055c64db604064e706ef5f5d
Denilson <denilson020898@gmail.com>
2024-01-04 20:48:37 +0700

    atomics

---
+++ c02_atomics/src/main.rs
@@ -0,0 +1,56 @@
+use std::{sync::atomic::{AtomicBool, AtomicUsize}, thread, time::Duration};
+use std::sync::atomic::Ordering::Relaxed;
+
+
+fn main() {
+    // static STOP: AtomicBool = AtomicBool::new(false);
+    // let background_thread = thread::spawn(|| {
+    //     while !STOP.load(std::sync::atomic::Ordering::Relaxed) {
+    //         some_work();
+    //     }
+    // });
+    // for line in std::io::stdin().lines() {
+    //     match line.unwrap().as_str() {
+    //         "help" => println!("commands: help, stop"),
+    //         "stop" => break,
+    //         cmd => println!("unknown command: {cmd:?}"),
+    //     }
+    // }
+    // STOP.store(true, std::sync::atomic::Ordering::Relaxed);
+    // background_thread.join().unwrap();
+
+    let num_done = AtomicUsize::new(0);
+
+    let main_thread = thread::current();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            for i in 0..20 {
+                process_item(i); // take some time
+                num_done.store(i + 1, Relaxed);
+                main_thread.unpark(); // wake up the main thread
+            }
+        });
+
+        loop {
+            let n = num_done.load(Relaxed);
+            if n == 20 { break; }
+            println!("working.. {n}/20 done");
+            // thread::sleep(Duration::from_secs(1));
+            thread::park_timeout(Duration::from_secs(1));
+        }
+
+    });
+
+    println!("completed");
+}
+
+fn some_work() {
+    // println!("some work");
+    // thread::sleep(Duration::from_secs(1));
+}
+
+fn process_item(inp: usize) -> usize {
+    std::thread::sleep(Duration::from_secs(2));
+    inp + 1
+}
