a771345b64a5cb607a5cf8a1c4ccbd519da08101
denilson <denilson020898@gmail.com>
2024-01-04 13:15:20 +0700

    mutex lock

--- c01_playground/src/main.rs
+++ c01_playground/src/main.rs
@@ -1,6 +1,70 @@
-use std::{cell::{Cell, RefCell}, rc::Rc, sync::Arc, thread, borrow::BorrowMut};
+use std::{
+    borrow::BorrowMut,
+    cell::{Cell, RefCell},
+    marker::PhantomData,
+    rc::Rc,
+    sync::{Arc, Mutex},
+    thread, time::Duration,
+};
 
 static X: [i32; 3] = [1, 2, 3];
+
+fn f() -> bool {
+    println!("hello from another thread!");
+    let id = thread::current().id();
+
+    let binding = thread::current();
+    let name = binding.name();
+    println!("from child id: {id:?} and {name:?}");
+    return false;
+}
+
+// in this example, a and b can not be the same reference
+fn f2(a: &i32, b: &mut i32) {
+    let before = *a;
+    *b += 1;
+    let after = *a;
+    if before != after {
+        x(); // never happens
+    }
+}
+
+fn x() {}
+
+// in this example, a and b can be the same reference
+// in the same thread
+fn f3(a: &Cell<i32>, b: &Cell<i32>) {
+    let before = a.get();
+    b.set(b.get() + 1);
+    let after = a.get();
+    if before != after {
+        x(); //might happen
+    }
+}
+
+// move out the value from cell and replace it
+// cell does not allow you to borrow its content
+fn f4(v: &Cell<Vec<i32>>) {
+    let mut v2 = v.take(); //replace with empty
+    v2.push(1);
+    v.set(v2);
+}
+
+fn f5(v: &RefCell<Vec<i32>>) {
+    v.borrow_mut().push(1); // only in single thread
+}
+
+struct X {
+    handle: i32,
+    _not_sync: PhantomData<Cell<()>>,
+}
+
+struct XTwo {
+    p: *mut i32,
+}
+
+unsafe impl Send for XTwo {}
+unsafe impl Sync for XTwo {}
 
 fn main() {
     // test 1
@@ -115,49 +179,52 @@ fn main() {
     let a = [123, 456, 789];
     let b = unsafe { a.get_unchecked(5) };
     println!("b is random value outside of the length of a: {b}");
-}
 
-fn f() -> bool {
-    println!("hello from another thread!");
-    let id = thread::current().id();
+    // let a = Rc::new(123);
+    // Rc is not 'Send'
+    // thread::spawn(move || {
+    //     dbg!(a);
+    // }).join();
 
-    let binding = thread::current();
-    let name = binding.name();
-    println!("from child id: {id:?} and {name:?}");
-    return false;
-}
 
-// in this example, a and b can not be the same reference
-fn f2(a: &i32, b: &mut i32) {
-    let before = *a;
-    *b += 1;
-    let after = *a;
-    if before != after {
-        x(); // never happens
-    }
-}
+    // thread A lock -> Success -> Returns MutexGuard (impl DerefMut) -> Ops -> Drop MutexGuard -> unlocked -> woke other thread
+    // thread B lock -> Blocked ------------------------------------------------------------------------------------------------> lock -> Returns MutexGuard -> ...
+    // let n = Mutex::new(0);
+    // thread::scope(|s| {
+    //     for _ in 0..10 {
+    //         s.spawn(|| {
+    //             let mut guard = n.lock().unwrap();
+    //             let threadid = thread::current().id();
+    //             for _ in 0..100 {
+    //                 // println!("updating inner 'n' from thread id : {threadid:?}");
+    //                 *guard += 1;
+    //             }
+    //
+    //             thread::sleep(Duration::from_secs(1));
+    //             // unlock the guard after this line 
+    //         });
+    //     }
+    // });
+    // assert_eq!(n.into_inner().unwrap(), 1000);
 
-fn x() {}
 
-// in this example, a and b can be the same reference
-// in the same thread
-fn f3(a: &Cell<i32>, b: &Cell<i32>) {
-    let before = a.get();
-    b.set(b.get() + 1);
-    let after = a.get();
-    if before != after {
-        x(); //might happen
-    }
-}
+    let n = Mutex::new(0);
+    thread::scope(|s| {
+        for _ in 0..10 {
+            s.spawn(|| {
+                let mut guard = n.lock().unwrap();
+                let threadid = thread::current().id();
+                for _ in 0..100 {
+                    println!("updating inner 'n' from thread id : {threadid:?}");
+                    *guard += 1;
+                }
 
-// move out the value from cell and replace it
-// cell does not allow you to borrow its content
-fn f4(v: &Cell<Vec<i32>>) {
-    let mut v2 = v.take(); //replace with empty
-    v2.push(1);
-    v.set(v2);
-}
-
-fn f5(v: &RefCell<Vec<i32>>) { 
-    v.borrow_mut().push(1);  // only in single thread
+                drop(guard); // drop guard before delaying, unlock the mutex here
+                // the sleeping and unlock + wake other thread is happening at the same time
+                thread::sleep(Duration::from_secs(1));
+                // unlock the guard after this line 
+            });
+        }
+    });
+    assert_eq!(n.into_inner().unwrap(), 1000);
 }
