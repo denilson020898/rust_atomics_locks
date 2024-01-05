ea9784bff28334e0671762c11916f5647430dc21
denilson <denilson020898@gmail.com>
2024-01-05 12:18:16 +0700

    atomic compare exchange

--- c02_atomics/src/main.rs
+++ c02_atomics/src/main.rs
@@ -1,8 +1,14 @@
-use std::{sync::atomic::{AtomicBool, AtomicUsize}, thread, time::Duration};
+use core::panic;
+use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
+use std::time::Instant;
+use std::{
+    sync::atomic::{AtomicBool, AtomicU64, AtomicUsize},
+    thread,
+    time::Duration,
+};
 
-const TO_PROCESS: usize = 50;
-
+const TO_PROCESS: usize = 100;
 
 fn main() {
     // static STOP: AtomicBool = AtomicBool::new(false);
@@ -21,30 +27,92 @@ fn main() {
     // STOP.store(true, std::sync::atomic::Ordering::Relaxed);
     // background_thread.join().unwrap();
 
-    let num_done = AtomicUsize::new(0);
+    // let num_done = AtomicUsize::new(0);
+    //
+    // let main_thread = thread::current();
+    //
+    // thread::scope(|s| {
+    //     s.spawn(|| {
+    //         for i in 0..TO_PROCESS {
+    //             process_item(i); // take some time
+    //             num_done.store(i + 1, Relaxed);
+    //             main_thread.unpark(); // wake up the main thread
+    //         }
+    //     });
+    //
+    //     loop {
+    //         let n = num_done.load(Relaxed);
+    //         if n == TO_PROCESS { break; }
+    //         println!("working.. {n}/{TO_PROCESS} done");
+    //         // thread::sleep(Duration::from_secs(1));
+    //         thread::park_timeout(Duration::from_secs(1));
+    //     }
+    //
+    // });
+    //
+    // println!("completed");
 
-    let main_thread = thread::current();
+    // let num_done = &AtomicUsize::new(0);
+    // thread::scope(|s| {
+    //     for t in 0..4 {
+    //         s.spawn(move || {
+    //             for i in 0..25 {
+    //                 process_item(t * 25 + i); // take some time
+    //                 num_done.fetch_add(1, Relaxed);
+    //             }
+    //         });
+    //     }
+    //
+    //     loop {
+    //         let n = num_done.load(Relaxed);
+    //         if n == TO_PROCESS {
+    //             break;
+    //         }
+    //         println!("working.. {n}/{TO_PROCESS} done");
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    // let n = num_done.load(Relaxed);
+    // println!("completed with {}", n);
 
-    thread::scope(|s| {
-        s.spawn(|| {
-            for i in 0..TO_PROCESS {
-                process_item(i); // take some time
-                num_done.store(i + 1, Relaxed);
-                main_thread.unpark(); // wake up the main thread
-            }
-        });
-
-        loop {
-            let n = num_done.load(Relaxed);
-            if n == TO_PROCESS { break; }
-            println!("working.. {n}/{TO_PROCESS} done");
-            // thread::sleep(Duration::from_secs(1));
-            thread::park_timeout(Duration::from_secs(1));
+    let num_done = &AtomicUsize::new(0);
+    let total_time = &AtomicU64::new(0);
+    let max_time = &AtomicU64::new(0);
+    thread::scope(|scope| {
+        for t in 0..4 {
+            scope.spawn(move || {
+                for i in 0..25 {
+                    let start = Instant::now();
+                    process_item(t * 25 + i);
+                    let time_taken = start.elapsed().as_micros() as u64;
+                    num_done.fetch_add(1, Relaxed);
+                    total_time.fetch_add(time_taken, Relaxed);
+                    max_time.fetch_max(time_taken, Relaxed);
+                }
+            });
         }
 
+        loop {
+            let total_time = Duration::from_micros(total_time.load(Relaxed));
+            let max_time = Duration::from_micros(max_time.load(Relaxed));
+            let n = num_done.load(Relaxed);
+            if n == TO_PROCESS {
+                break;
+            }
+            if n == 0 {
+                println!("gabut bos");
+            } else {
+                println!(
+                    "Working.. {n}/100 done, {:?} average, {:?} peak",
+                    total_time / n as u32,
+                    max_time
+                );
+            }
+            thread::sleep(Duration::from_secs(1));
+        }
     });
-
-    println!("completed");
+    let n = num_done.load(Relaxed);
+    println!("completed with {}", n);
 }
 
 fn some_work() {
@@ -53,6 +121,42 @@ fn some_work() {
 }
 
 fn process_item(inp: usize) -> usize {
-    std::thread::sleep(Duration::from_secs(2));
+    dbg!(inp);
+    std::thread::sleep(Duration::from_secs(1));
     inp + 1
+}
+
+fn get_x() -> u64 {
+    static X: AtomicU64 = AtomicU64::new(0);
+    let mut x = X.load(Relaxed);
+    if x == 0 {
+        x = calculate_x();
+        X.store(x, Relaxed);
+    }
+    x
+}
+
+fn calculate_x() -> u64 {
+    todo!()
+}
+
+fn allocate_new_id() -> u32 {
+    static NEXT_ID: AtomicU32 = AtomicU32::new(0);
+    let id = NEXT_ID.fetch_add(1, Relaxed);
+    if id >= 1000 {
+        NEXT_ID.fetch_sub(1, Relaxed);
+        panic!("too many ids!");
+    }
+    return id;
+}
+
+fn increment(a: &AtomicU32) {
+    let mut current = a.load(Relaxed);
+    loop {
+        let new = current + 1;
+        match a.compare_exchange(current, new, Relaxed, Relaxed) {
+            Ok(_) => return,
+            Err(v) => current = v,
+        }
+    }
 }
---
+++ z03_channels/src/main.rs
@@ -0,0 +1,69 @@
+use std::{sync::mpsc, thread, time::Duration};
+
+fn main() {
+    // let (tx, rx) = mpsc::channel();
+    // let t1 = tx.clone();
+    // let t2 = tx.clone();
+    // let t3 = tx.clone();
+    //
+    // let th1 = thread::spawn(move || {
+    //     thread::sleep(Duration::from_secs(5));
+    //     t1.send(50).unwrap();
+    //     println!("thread 1 completed: 50");
+    // });
+    //
+    // let th2 = thread::spawn(move || {
+    //     thread::sleep(Duration::from_secs(2));
+    //     t2.send(123).unwrap();
+    //     println!("thread 2 completed: 123");
+    // });
+    //
+    // let th3 = thread::spawn(move || {
+    //     thread::sleep(Duration::from_secs(10));
+    //     t3.send(666).unwrap();
+    //     println!("thread 3 completed: 666");
+    // });
+    //
+    // let th4 = thread::spawn(move || {
+    //     let result = rx.recv().unwrap() + rx.recv().unwrap() + rx.recv().unwrap();
+    //     println!("total: {result}");
+    // });
+    //
+    // th1.join().unwrap();
+    // th2.join().unwrap();
+    // th3.join().unwrap();
+    // th4.join().unwrap();
+    //
+    // println!("All threads completed!");
+
+    thread::scope(|scope| {
+        let (tx, rx) = mpsc::channel();
+        let t1 = tx.clone();
+        let t2 = tx.clone();
+        let t3 = tx.clone();
+
+        scope.spawn(move || {
+            thread::sleep(Duration::from_secs(5));
+            t1.send(50).unwrap();
+            println!("thread 1 completed: 50");
+        });
+
+        scope.spawn(move || {
+            thread::sleep(Duration::from_secs(2));
+            t2.send(123).unwrap();
+            println!("thread 2 completed: 123");
+        });
+
+        scope.spawn(move || {
+            thread::sleep(Duration::from_secs(10));
+            t3.send(666).unwrap();
+            println!("thread 3 completed: 666");
+        });
+
+        scope.spawn(move || {
+            let result = rx.recv().unwrap() + rx.recv().unwrap() + rx.recv().unwrap();
+            println!("total: {result}");
+        });
+    });
+    println!("All threads completed!");
+}
